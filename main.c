540599d4d78c54fe80c071da475fd5c8864c7e8d
Jonas Fluhrer <fluhrer@hochwarth-it.de>
2019-01-21 14:21:36 +0100

    added menu

--- main.c
+++ main.c
@@ -3,16 +3,29 @@
 #include <strings.h>
 #include <ctype.h>
 
+#define AE (unsigned char)142
+#define ae (unsigned char)132
+#define OE (unsigned char)153
+#define oe (unsigned char)148
+#define UE (unsigned char)154
+#define ue (unsigned char)129
+#define ss (unsigned char)225
+
 void searchBook(){
+    printf("searchBook");
 }
 void borrowBook(){
+    printf("borrowBook");
 }
-void givebackBook(){
+void returnBook(){
+    printf("returnBook");
 }
 void addBook(){
     // add a new copy of an existing book
+    printf("addBook");
 }
 void newBook(){
+    printf("newBook");
     // initialize variables
     char isbn[256], newisbn[256], bookTitle[256], bookAuthor[256];
     int bookCount;
@@ -53,7 +66,40 @@ void newBook(){
 void deleteBook(){
 }
 
+void showMenu(){
+    char input;
+    printf("[S]: B%ccher suchen\n", ue);
+    printf("[A]: Buch ausleihen\n");
+    printf("[Z]: Buch zur%cckgeben\n", ue);
+    printf("[H]: Buch hinzuf%cgen\n", ue);
+    printf("[L]: Buch l%cschen\n", oe);
+    printf("Bitte w%chle eine Aktion von oben: ", ae);
+    scanf("%c", &input);
+    input = tolower(input);
+    switch(input){
+        case 's':
+            searchBook();
+            break;
+        case 'a':
+            addBook();
+            break;
+        case 'z':
+            returnBook();
+            break;
+        case 'h':
+            addBook();
+            break;
+        case 'l':
+            deleteBook();
+            break;
+        default:
+            printf("Falsche Eingabe!\n");
+            showMenu();
+            break;
+    }
+}
+
 int main()
 {
-    newBook();
+    showMenu();
 }
