e0914fdc2b5887482d11f8662dabb6d0a84f959c
Veronika952 <veronika.fischer.germany@gmail.com>
2019-02-16 17:45:29 +0100

    fgets bookTitle hinzugefügt

--- deleteBooks.c
+++ deleteBooks.c
@@ -1,7 +1,15 @@
 #include "deleteBooks.h"
+
+
+
 void deleteBooks(Array *books)
 {
+    char bookTitle [80];
+    //which book shall be deleted?
     printf("Welches Buch soll gelöscht werden?\n");
+    fgets(bookTitle, 80, stdin);
+   // hier mit suchstring verknüpfen
+
 
 }
 
